a91f9549f8d77a0858a51aa632ff75d76e6bf0e1
R <2085803@cognizant.com>
2022-05-09 15:32:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const secondHand=document.querySelector('.sechand');
+const minsHand=document.querySelector('.min-hand');
+const hourHand=document.querySelector('.hour-hand');
+function setDate(){
+    const now=new Date();
+    const seconds=now.getSeconds();
+
+
+    const secondDegrees=((seconds/60)*360)+90;
+    secondHand.style.transform=`rotate(${secondDegrees}deg)`;
+
+
+    const mins=now.getMinutes();
+    const minsDegrees=((mins/60)*360)+90;
+    minsHand.style.transform=`rotate(${minsDegrees}deg)`;
+
+    
+    const hours=now.getHours();
+    const hourDegrees=((hours/12)*360)+90;
+    hourHand.style.transform=`rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate,1000);
